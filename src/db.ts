8938e5d9fe6d7797ecd9f839e8b96bfb20bc0ee6
Burhan Syed <busyed98@gmail.com>
2023-12-04 20:17:21 -0500

    connect to db and establish schema

---
+++ src/db.ts
@@ -0,0 +1,60 @@
+import mysql from 'mysql2/promise'; 
+
+const DATABASE_HOST = process.env.DATABASE_HOST; 
+const DATABASE_PORT = process.env.DATABASE_PORT; 
+const DATABASE_USER = process.env.DATABASE_USER;
+const DATABASE_PASSWORD = process.env.DATABASE_PASSWORD; 
+
+const DATABASE = 'ganymede_QA'
+
+if (!DATABASE_HOST || !DATABASE_PORT || !DATABASE_USER || !DATABASE_PASSWORD) {
+  throw new Error('missing database credentials'); 
+}
+
+const db = mysql.createPool({
+  host: DATABASE_HOST, 
+  user: DATABASE_USER, 
+  port: +DATABASE_PORT, 
+  password: DATABASE_PASSWORD,
+  waitForConnections: true
+});
+
+db.query(`CREATE DATABASE IF NOT EXISTS ${DATABASE}`)
+ .then(() => db.query(`USE ${DATABASE}`))
+ .then(() => db.query(`CREATE TABLE IF NOT EXISTS questions (
+  id INT NOT NULL AUTO_INCREMENT, 
+  body TEXT,
+  date_written DATETIME DEFAULT CURRENT_TIMESTAMP,
+  asker_name VARCHAR(64),
+  asker_email VARCHAR(64),
+  reported BOOLEAN NOT NULL DEFAULT FALSE,
+  helpful INT NOT NULL DEFAULT 0,
+
+  PRIMARY KEY (id)
+ )`))
+ .then(() => db.query(`CREATE TABLE IF NOT EXISTS answers (
+  id INT NOT NULL AUTO_INCREMENT, 
+  question_id INT NOT NULL,
+  body TEXT,
+  date_written DATETIME DEFAULT CURRENT_TIMESTAMP,
+  answerer_name VARCHAR(64),
+  answerer_email VARCHAR(64),
+  reported BOOLEAN NOT NULL DEFAULT FALSE,
+  helpful INT NOT NULL DEFAULT 0,
+
+  PRIMARY KEY (id),
+  FOREIGN KEY (question_id) REFERENCES questions(id)
+ )`))
+ .then(() => db.query(`CREATE TABLE IF NOT EXISTS answer_photos (
+  id INT NOT NULL AUTO_INCREMENT, 
+  answer_id INT NOT NULL, 
+  url TEXT NOT NULL, 
+
+  PRIMARY KEY (id), 
+  FOREIGN KEY (answer_id) REFERENCES answers(id)
+ )`))
+ .then(() => console.log('connected to', DATABASE));
+
+
+
+export default db; 
